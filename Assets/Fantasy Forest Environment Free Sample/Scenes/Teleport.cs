0cd71c4988e27e333dfed059e6f9d66f7ac530e0
Kauam <kauamsilva360@gmail.com>
2023-05-01 23:37:03 -0300

    up

---
+++ Assets/Fantasy Forest Environment Free Sample/Scenes/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public void TeleportPlayer(Transform pos)
+    {
+        transform.position = pos.position;
+    }
+
+    public void mata(GameObject enn)
+    {
+        Destroy(enn);
+        
+    }
+
+    public void laseentra(GameObject laser)
+    {
+        laser.SetActive(true);
+    }
+
+    public void lasesai(GameObject laser)
+    {
+        laser.SetActive(false);
+    }
+
+}
