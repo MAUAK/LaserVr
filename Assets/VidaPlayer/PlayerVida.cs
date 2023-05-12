301123d232459f22b75be4ab0aac0acff7a0736b
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2023-05-12 15:35:17 -0300

    VIda

---
+++ Assets/VidaPlayer/PlayerVida.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerVida : MonoBehaviour
+{
+    public float Health;
+    public Slider HealthBar;
+
+
+    void Start()
+    {
+        HealthBar.value = Health;
+    }
+
+    void Update()
+    {
+        if (Health == 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer.Equals(8))
+        {
+            Health -= 10;
+            HealthBar.value = Health;
+        }
+    }
+}
