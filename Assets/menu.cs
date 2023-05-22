541a1ca3c713b960b8130b980da8a2f06e8499a2
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2023-05-21 22:47:25 -0300

    telas do game

---
+++ Assets/menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+   public void iniciarGame()
+    {
+        SceneManager.LoadScene("demoScene_free");
+    }
+
+   public void QuitGame()
+    {
+        Debug.Log("Saindo do jogo!");
+        Application.Quit();
+    }
+}
